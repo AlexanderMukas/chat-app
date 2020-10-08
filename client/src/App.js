565d97bda386dc54a040d04597b3b4cd48be0a78
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-08 22:50:39 +0300

    some errors commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route} from ' ';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 // components 
 import Join from './components/Join';
