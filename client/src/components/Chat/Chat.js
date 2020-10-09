9ef404231847252770382986b35ee422c5adb30b
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-10 00:12:32 +0300

    connet io in Chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -6,21 +6,28 @@ import "./Chat.css";
 
 
 
-
+let socket;
 
 
 const Chat = ( { location } ) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
 
     useEffect( () => {
     //    const data = queryString.parse(location.search);
     //    console.log(data);                  // Object { name: "Alexander", room: "room1" }
     //    console.log(location.search);       // ?name=Alexander&room=room1
         const {name, room} = queryString.parse(location.search);
+        
+
+        socket = io(ENDPOINT);     // ENDPOINT, serverside
+
         //assign data 
         setName(name);
         setRoom(room);
+
+        console.log(socket);
 
     });
 
