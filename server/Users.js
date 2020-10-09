7110a1148dba30e22e6913cc01c4c2dfcef5c2ea
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-10 00:59:09 +0300

    addUser done commit

--- server/Users.js
+++ server/Users.js
@@ -1,20 +1,35 @@
 // create helper functions: addUser, removeUser, getUser, getUsersInRoom
 
+const e = require("express");
+
 const users = [];
 
-const addUser = () => {
+const addUser = ( {id, name, room}) => {
+    // If name have 2 words : Alexander Mukas =alexandermukas
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+    // check if user exist
+    const existingUser = users.find( user => user.room === room && user.name === name);
+    if(existingUser) {
+        return { error : 'Username is taken'};
+    };
+    
+    const user = { id, name, room};
+
+    // add new User
+    users.push(user);
+    return { user };
+}
+
+const removeUser = ( {id, name}) => {
 
 }
 
-const removeUser = () => {
+
+const getUser = ( {name} ) => {
 
 }
 
-
-const getUser = () => {
-
-}
-
-const getUsersInRoom = () => {
+const getUsersInRoom = ( {room} ) => {
 
 }
