26d5f1c3f615b1d183c96eef36afb6611399fc02
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-10 00:07:44 +0300

    test version of Chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -10,15 +10,26 @@ import "./Chat.css";
 
 
 const Chat = ( { location } ) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     useEffect( () => {
-       const data = queryString.parse(location.search);
-       console.log(data);
+    //    const data = queryString.parse(location.search);
+    //    console.log(data);                  // Object { name: "Alexander", room: "room1" }
+    //    console.log(location.search);       // ?name=Alexander&room=room1
+        const {name, room} = queryString.parse(location.search);
+        //assign data 
+        setName(name);
+        setRoom(room);
 
     });
 
 
     return (
-        <h1>This is Chat</h1>
+        <>
+            <h1>This is Chat</h1>
+            <h2>User: {name} , Room: {room}</h2>
+        </>
     );
 };
 
