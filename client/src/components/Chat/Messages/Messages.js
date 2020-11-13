27d5cbb1415df00a04b0ace8d98f6f55965414b9
AlexanderMukas <alexandermukas@yahoo.com>
2020-11-14 01:41:33 +0200

    create messages component commit

--- client/src/components/Chat/Messages/Messages.js
+++ client/src/components/Chat/Messages/Messages.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
-const Messages = () => {
+import './Messages.css';
 
-};
+const Messages = ({ messages }) => (
+    <ScrollToBottom>
+        {messages.map(
+            (message, index) => <div key={index}>
+                <Message />
+            </div>
+        )}
+    </ScrollToBottom>
+);
+
+
 
 export default Messages;
