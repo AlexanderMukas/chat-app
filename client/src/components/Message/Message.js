1b4464fda5db81798fcf7e5ae7d84ef56e3751ec
AlexanderMukas <alexandermukas@yahoo.com>
2020-11-14 02:13:41 +0200

    some message commit

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -17,10 +17,10 @@ const Message = ({ message: {user , text}, name }) => {
         isSentByCurrentUser
             ? (
                 // true
-                <div className="messageContainer">
-                    <p className="sentText">{trimmedName}</p>
-                    <div className="messageBox">
-                        <p className="messageText">{text}</p>
+                <div className="messageContainer justifyEnd">
+                    <p className="sentText pr-10">{trimmedName}</p>
+                    <div className="messageBox backgroundBlue">
+                        <p className="messageText colorWhite">{text}</p>
 
                     </div>
 
@@ -28,6 +28,14 @@ const Message = ({ message: {user , text}, name }) => {
             )
             : (
                 // false
+                <div className="messageContainer justifyStart">
+                    
+                    <div className="messageBox backgroundLight">
+                        <p className="messageText colorDark">{text}</p>
+
+                    </div>
+                    <p className="sentText pl-10">{user}</p>
+                </div>
             )
     )
 
