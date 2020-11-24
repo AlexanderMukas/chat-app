7e87a1ccb22252b2b7352eb201323f07232aaf32
AlexanderMukas <alexandermukas@yahoo.com>
2020-11-24 23:36:03 +0200

    some commit

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -25,11 +25,7 @@ const Message = ({ message: {user , text}, name }) => {
                 <div className="messageContainer justifyEnd">
                     <p className="sentText pr-10">{trimmedName}</p>
                     <div className="messageBox backgroundBlue">
-
-                        {/* <p className="messageText colorWhite">{text}</p> */}
-
                         <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
-
                     </div>
 
                 </div>
@@ -39,7 +35,6 @@ const Message = ({ message: {user , text}, name }) => {
                 <div className="messageContainer justifyStart">
                     
                     <div className="messageBox backgroundLight">
-                        {/* <p className="messageText colorDark">{text}</p> */}
                         <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
                     </div>
                     
